4c0edbbfe26c245691bbc66d3601b03a5f86dc94
Steven Masley <stevenmasley@gmail.com>
2017-01-11 21:28:25 -0600

    Fixed deployment nodes structure

--- compileWin.sh
+++
@@ -1,6 +0,0 @@
-echo "Compiling for windows"
-GOOS=windows GOARCH=amd64 go build
-# GOOS=windows GOARCH=386 go build -v
-# echo "Moving to folder"
-# rm windows-binaries/enterprise-wallet.exe
-# mv enterprise-wallet.exe windows-binaries/enterprise-wallet.exe
