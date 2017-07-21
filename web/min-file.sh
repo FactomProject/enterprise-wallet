a8dea010d6db716e53ea57511da5a44dd1973dbd
Steven Masley <stevenmasley@gmail.com>
2017-07-21 12:19:44 -0500

    Removed minify dependency

--- web/min-file.sh
+++ web/min-file.sh
@@ -14,26 +14,23 @@ sh non-essentials.sh
 echo "Minfying templates..."
 # minify -r -o min-templates templates/
 echo "Minfying javascript..."
-if [ "$1" == "closure" ] # Closure compile by google
+if [ "$1" = "closure" ] # Closure compile by google
 	then
 	#for filename in statics/js/*; do
 	#	echo "  Minifying ${filename}..."
 	#	java -jar closure/compiler.jar  --js_output_file=min-${filename} ${filename} #--compilation_level=ADVANCED
 	#done
-	echo "  Minifying all.js..."
+	echo "  Minifying all.js using closure..."
 	java -jar closure/compiler.jar  --js_output_file=min-statics/js/all.js statics/js/all.js
-	echo "  Minifying ajax.js..."
+	echo "  Minifying ajax.js using closure..."
 	java -jar closure/compiler.jar  --js_output_file=min-statics/js/ajax.js statics/js/ajax.js
-	echo "  Minifying app.js..."
+	echo "  Minifying app.js using closure..."
 	java -jar closure/compiler.jar  --js_output_file=min-statics/js/app.js statics/js/app.js
-elif [ "$1" == "none" ] 
-	then
-	echo "No minify"
 else
-	minify -r -o min-statics/js statics/js/
+	echo "No minify"
 fi
 
-if [ "$1" == "none" ] 
+if [ "$1" = "none" ] 
 	then
 	echo "Not minify css"
 else 
